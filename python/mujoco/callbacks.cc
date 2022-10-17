google-deepmind/mujoco

commit ec6ea6a69e43b40ab3ef30981408f9baccaea047

Fix a memory leak when using Python callbacks.
Fixes #527.

PiperOrigin-RevId: 481173711
Change-Id: Ie176a8bda09727b6be8b6641d3be9a75c675d566

python/mujoco/callbacks.cc
@@ -46,7 +46,7 @@ using enable_if_not_const_t =
 // table that associates raw MuJoCo struct pointers back to the pointers to
 // their corresponding wrappers.
 template <typename Raw>
-static enable_if_not_const_t<Raw, py::handle> MjWrapperLookup(Raw* ptr) {
+static enable_if_not_const_t<Raw, py::object> MjWrapperLookup(Raw* ptr) {
   using LookupFnType = MjWrapper<Raw>* (Raw*);
   static LookupFnType* const lookup = []() -> LookupFnType* {
     py::gil_scoped_acquire gil;
@@ -93,8 +93,8 @@ static enable_if_not_const_t<Raw, py::handle> MjWrapperLookup(Raw* ptr) {
     const auto [src, type] =
         py::detail::type_caster_base<MjWrapper<Raw>>::src_and_type(wrapper);
     if (type) {
-      py::handle instance =
-          py::detail::find_registered_python_instance(wrapper, type);
+      py::object instance = py::reinterpret_steal<py::object>(
+          py::detail::find_registered_python_instance(wrapper, type));
       if (!instance) {
         if (!PyErr_Occurred()) {
           PyErr_SetString(
@@ -117,7 +117,7 @@ static enable_if_not_const_t<Raw, py::handle> MjWrapperLookup(Raw* ptr) {
 }
 
 template <typename Raw>
-static const py::handle MjWrapperLookup(const Raw* ptr) {
+static const py::object MjWrapperLookup(const Raw* ptr) {
   return MjWrapperLookup(const_cast<Raw*>(ptr));
 }
 
@@ -172,28 +172,28 @@ static PyObject* py_mju_user_free = nullptr;
 static PyObject* py_mjcb_passive = nullptr;
 static void PyMjcbPassive(const raw::MjModel* m, raw::MjData* d) {
   CallPyCallback<void>("mjcb_passive", py_mjcb_passive,
-                        MjWrapperLookup(m), MjWrapperLookup(d));
+                       MjWrapperLookup(m), MjWrapperLookup(d));
 }
 
 static PyObject* py_mjcb_control = nullptr;
 static void PyMjcbControl(const raw::MjModel* m, raw::MjData* d) {
   CallPyCallback<void>("mjcb_control", py_mjcb_control,
-                         MjWrapperLookup(m), MjWrapperLookup(d));
+                       MjWrapperLookup(m), MjWrapperLookup(d));
 }
 
 static PyObject* py_mjcb_contactfilter = nullptr;
 static int PyMjcbContactfilter(
     const raw::MjModel* m, raw::MjData* d, int geom1, int geom2) {
   return CallPyCallback<int>("mjcb_contactfilter", py_mjcb_contactfilter,
-                               MjWrapperLookup(m), MjWrapperLookup(d),
-                               geom1, geom2);
+                             MjWrapperLookup(m), MjWrapperLookup(d),
+                             geom1, geom2);
 }
 
 static PyObject* py_mjcb_sensor = nullptr;
 static void
 PyMjcbSensor(const raw::MjModel* m, raw::MjData* d, int stage) {
   CallPyCallback<void>("mjcb_sensor", py_mjcb_sensor,
-                         MjWrapperLookup(m), MjWrapperLookup(d), stage);
+                       MjWrapperLookup(m), MjWrapperLookup(d), stage);
 }
 
 static PyObject* py_mjcb_time = nullptr;

python/mujoco/bindings_test.py
@@ -962,6 +962,23 @@ Euler integrator, semi-implicit in velocity.
     self.assertEqual(sensor_callback.count, 1)
     self.assertEqual(data_with_sensor.sensordata[0], 17)
 
+  def test_mjcb_control_not_leak_memory(self):
+    model_instances = []
+    data_instances = []
+    for _ in range(10):
+      mujoco.set_mjcb_control(None)
+      model_instances.append(mujoco.MjModel.from_xml_string('<mujoco/>'))
+      data_instances.append(mujoco.MjData(model_instances[-1]))
+      mujoco.set_mjcb_control(lambda m, d: None)
+      mujoco.mj_step(model_instances[-1], data_instances[-1])
+    mujoco.set_mjcb_control(None)
+    while data_instances:
+      d = data_instances.pop()
+      self.assertEqual(sys.getrefcount(d), 2)
+    while model_instances:
+      m = model_instances.pop()
+      self.assertEqual(sys.getrefcount(m), 2)
+
   def test_can_initialize_mjv_structs(self):
     self.assertIsInstance(mujoco.MjvScene(), mujoco.MjvScene)
     self.assertIsInstance(mujoco.MjvCamera(), mujoco.MjvCamera)

commit 3aaf94f42c206de95b497d388be35bb509434ff0

Fix Python GIL-not-held issues in `python/callbacks.cc`.
The GIL bug was introduced in ec6ea6a69e43b40ab3ef30981408f9baccaea047 and is resolved by decrementing the refcount before returning from MjWrapperLookup.

PiperOrigin-RevId: 481558798
Change-Id: I2744287d48635a55ae6d2ec46c9e43fe881019d8

python/mujoco/callbacks.cc
@@ -18,6 +18,7 @@
 #include <limits>
 #include <sstream>
 #include <type_traits>
+#include <utility>
 
 #include <mujoco/mujoco.h>
 #include "errors.h"
@@ -46,7 +47,7 @@ using enable_if_not_const_t =
 // table that associates raw MuJoCo struct pointers back to the pointers to
 // their corresponding wrappers.
 template <typename Raw>
-static enable_if_not_const_t<Raw, py::object> MjWrapperLookup(Raw* ptr) {
+static enable_if_not_const_t<Raw, py::handle> MjWrapperLookup(Raw* ptr) {
   using LookupFnType = MjWrapper<Raw>* (Raw*);
   static LookupFnType* const lookup = []() -> LookupFnType* {
     py::gil_scoped_acquire gil;
@@ -102,7 +103,7 @@ static enable_if_not_const_t<Raw, py::object> MjWrapperLookup(Raw* ptr) {
               "cannot find the Python instance of the MjWrapper");
         }
       } else {
-        return instance;
+        return std::move(instance);
       }
     } else {
       if (!PyErr_Occurred()) {
@@ -117,7 +118,7 @@ static enable_if_not_const_t<Raw, py::object> MjWrapperLookup(Raw* ptr) {
 }
 
 template <typename Raw>
-static const py::object MjWrapperLookup(const Raw* ptr) {
+static const py::handle MjWrapperLookup(const Raw* ptr) {
   return MjWrapperLookup(const_cast<Raw*>(ptr));
 }
 