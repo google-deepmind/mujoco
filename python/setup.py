9ffde761e886dd7dfa3f38b53f7f2f68ef46c00e
Nimrod Gileadi <nimrod@google.com>
2022-11-08 04:21:49 -0800

    Add support for Python 3.11 to MuJoCo build.
    PiperOrigin-RevId: 486911779
    Change-Id: I57a043ed9cbdf84cddf776f4654941370f050a8c

--- python/setup.py
+++ python/setup.py
@@ -310,6 +310,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Scientific/Engineering',
     ],
     cmdclass=dict(build_ext=BuildCMakeExtension),
