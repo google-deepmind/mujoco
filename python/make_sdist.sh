fd139276bceb15fd8943af90f914ad61ff136cfb
Saran Tunyasuvunakool <stunya@google.com>
2022-11-10 10:06:53 -0800

    Run `pip install` with `--require-hashes`.
    PiperOrigin-RevId: 487569781
    Change-Id: I0a810403f789415a3f884531617801ae111c957b

--- python/make_sdist.sh
+++ python/make_sdist.sh
@@ -28,8 +28,8 @@ else
   readonly tmp_dir="$(mktemp -d)"
 fi
 
-python -m pip install --upgrade pip setuptools
-python -m pip install absl-py
+python -m pip install --upgrade --require-hashes \
+    -r ${package_dir}/make_sdist_requirements.txt
 pushd ${tmp_dir}
 cp -r "${package_dir}"/* .
 
