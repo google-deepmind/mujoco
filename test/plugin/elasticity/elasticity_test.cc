17c82e2f02d3081bb25c58c001566b48c090cb0e
Alessio Quaglino <quaglino@google.com>
2022-11-17 07:38:16 -0800

    Automated g4 rollback of changelist 489183597.
    *** Reason for rollback ***
    
    GitHub path is different
    
    *** Original change description ***
    
    Activate plugin tests on Windows.
    
    ***
    
    PiperOrigin-RevId: 489207765
    Change-Id: If48dede94c8623b92526375a567edcd49c548e63

--- test/plugin/elasticity/elasticity_test.cc
+++ test/plugin/elasticity/elasticity_test.cc
@@ -27,28 +27,8 @@
 
 namespace mujoco {
 namespace {
+using PluginTest = MujocoTest;
 
-class PluginTest : public MujocoTest {
- public:
-  // load plugin library
-  PluginTest() : MujocoTest() {
-    #if defined(_WIN32) || defined(__CYGWIN__)
-      mj_loadPluginLibrary((
-        std::string(std::getenv("MUJOCO_PLUGIN_DIR")) +
-        std::string("\\elasticity.dll")).c_str());
-    #else
-      #if defined(__APPLE__)
-        mj_loadPluginLibrary((
-          std::string(std::getenv("MUJOCO_PLUGIN_DIR")) +
-          std::string("/libelasticity.dylib")).c_str());
-      #else
-        mj_loadPluginLibrary((
-          std::string(std::getenv("MUJOCO_PLUGIN_DIR")) +
-          std::string("/libelasticity.so")).c_str());
-      #endif
-    #endif
-  }
-};
 
 // -------------------------------- cable -----------------------------------
 
