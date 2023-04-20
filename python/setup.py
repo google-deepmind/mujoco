0630b7543dc4033cdaf7c680b8f9379f774bd8c7
Saran Tunyasuvunakool <stunya@google.com>
2023-04-20 07:52:03 -0700

    Bump version number to 2.3.4.
    PiperOrigin-RevId: 525745444
    Change-Id: I8a9994b72a0a2ba3f371bee2c2bea947ad8f4fd1

--- python/setup.py
+++ python/setup.py
@@ -32,7 +32,7 @@ from setuptools import setup
 from setuptools.command import build_ext
 from setuptools.command import install_scripts
 
-__version__ = '2.3.3'
+__version__ = '2.3.4'
 
 MUJOCO_CMAKE = 'MUJOCO_CMAKE'
 MUJOCO_CMAKE_ARGS = 'MUJOCO_CMAKE_ARGS'
--- src/engine/engine_support.c
+++ src/engine/engine_support.c
@@ -36,8 +36,8 @@
   #endif
 #endif
 
-#define mjVERSION 233
-#define mjVERSIONSTRING "2.3.3"
+#define mjVERSION 234
+#define mjVERSIONSTRING "2.3.4"
 
 // names of disable flags
 const char* mjDISABLESTRING[mjNDISABLE] = {
