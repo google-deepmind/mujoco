875823ba9607a85aeab2609044ff50df535ac2b7
Yuval Tassa <tassa@google.com>
2024-05-18 03:06:41 -0700

    Speed up slow test.
    PiperOrigin-RevId: 635014612
    Change-Id: Ifb7fe174827f59647bb254a3ece25abbd57962ca

--- test/sample/testspeed_test.sh
+++ test/sample/testspeed_test.sh
@@ -28,8 +28,12 @@ test_model() {
   local iterations=10
   # for particularly slow models, only run 2 steps under ASAN, or skip.
   if [[ ${TESTSPEED_ASAN:-0} != 0 ]]; then
-    if [[ "$model" == */composite/particle.xml ]]; then
-      # this test can take several minutes under ASAN
+    if [[ "$model" == */composite/particle.xml ||
+          "$model" == */replicate/bunnies.xml ||
+          "$model" == */replicate/leaves.xml ||
+          "$model" == */replicate/particle.xml
+    ]]; then
+      # these tests can take several minutes under ASAN
       return 0
     fi
     if [[ "$model" == */benchmark/testdata/humanoid200.xml ||
