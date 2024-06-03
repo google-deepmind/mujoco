decoupling tendons to subcomponents and fixing tendon scaling issue

python/mujoco/usd/exporter.py
@@ -17,9 +17,11 @@ import os
 import mujoco
 
 # import mujoco.usd.shapes as shapes_module
-# import mujoco.usd.component as component_module
+# import mujoco.usd.component as object_module
 import shapes as shapes_module
-import component as component_module
+import objects as object_module
+import lights as light_module
+import camera as camera_module
 
 import numpy as np
 import scipy
@@ -181,7 +183,6 @@ class USDExporter:
     # update the mujoco renderer
     self.renderer.update_scene(data, scene_option=scene_option)
 
-    # TODO: update scene options
     if self.updates == 0:
       self._initialize_usd_stage()
 
@@ -195,7 +196,6 @@ class USDExporter:
     self.updates += 1
 
   def _load_textures(self):
-    # TODO: remove code once added internally to mujoco
     data_adr = 0
     self.texture_files = []
     for texture_id in tqdm.tqdm(range(self.model.ntex)):
@@ -241,8 +241,9 @@ class USDExporter:
 
     texture_file = self.texture_files[geom.texid] if geom.texid != -1 else None
 
+    # handling meshes in our scene
     if geom.type == mujoco.mjtGeom.mjGEOM_MESH:
-      usd_geom = component_module.USDMesh(
+      usd_geom = object_module.USDMesh(
           stage=self.stage,
           model=self.model,
           geom=geom,
@@ -252,14 +253,15 @@ class USDExporter:
           texture_file=texture_file,
       )
     else:
+      # handling tendons in our scene
       if geom.objtype == mujoco.mjtObj.mjOBJ_TENDON:
-        # scale of a tendon is equivalent to its size, assume a unit length geom size
         mesh_config = shapes_module.mesh_config_generator(
             name=geom_name,
             geom_type=geom.type,
-            size=np.array([1.0, 1.0, 1.0])
+            size=np.array([1.0, 1.0, 1.0]),
+            decouple=True
         )
-        usd_geom = component_module.USDTendon(
+        usd_geom = object_module.USDTendon(
             mesh_config=mesh_config,
             stage=self.stage,
             geom=geom,
@@ -267,13 +269,14 @@ class USDExporter:
             rgba=geom.rgba,
             texture_file=texture_file,
         )
+      # handling primitives in our scene
       else:
         mesh_config = shapes_module.mesh_config_generator(
             name=geom_name,
             geom_type=geom.type,
             size=geom.size   
         )
-        usd_geom = component_module.USDPrimitiveMesh(
+        usd_geom = object_module.USDPrimitiveMesh(
             mesh_config=mesh_config,
             stage=self.stage,
             geom=geom,
@@ -298,7 +301,6 @@ class USDExporter:
 
       if geom.objtype == mujoco.mjtObj.mjOBJ_TENDON:
         tendon_scale = geom.size
-        tendon_scale[2] /= 2
         self.geom_refs[geom_name].update(
             pos=geom.pos,
             mat=geom.mat,
@@ -321,7 +323,7 @@ class USDExporter:
       light = self.scene.lights[i]
       if not np.allclose(light.pos, [0, 0, 0]):
         self.usd_lights.append
-        (component_module.USDSphereLight(stage=self.stage, obj_name=str(i)))
+        (light_module.USDSphereLight(stage=self.stage, obj_name=str(i)))
       else:
         self.usd_lights.append(None)
 
@@ -347,7 +349,7 @@ class USDExporter:
     if self.camera_names is not None:
       for name in self.camera_names:
         self.usd_cameras.append(
-            component_module.USDCamera(stage=self.stage, obj_name=name))
+            camera_module.USDCamera(stage=self.stage, obj_name=name))
 
   def _update_cameras(
       self,
@@ -388,11 +390,11 @@ class USDExporter:
   ):
 
     if light_type == "sphere":
-      new_light = component_module.USDSphereLight(stage=self.stage, obj_name=str(objid), radius=radius)
+      new_light = light_module.USDSphereLight(stage=self.stage, obj_name=str(objid), radius=radius)
 
       new_light.update(pos=np.array(pos), intensity=intensity, color=color, frame=0)
     elif light_type == "dome":
-      new_light = component_module.USDDomeLight(
+      new_light = light_module.USDDomeLight(
           stage=self.stage, obj_name=str(objid))
 
       new_light.update(intensity=intensity, color=color, frame=0)
@@ -403,7 +405,7 @@ class USDExporter:
       rotation_xyz: List[float],
       obj_name: Optional[str] = "camera_1",
   ):
-    new_camera = component_module.USDCamera(
+    new_camera = camera_module.USDCamera(
         stage=self.stage, obj_name=str(objid))
 
     r = scipy.spatial.transform.Rotation.from_euler(

python/mujoco/usd/shapes.py
@@ -27,14 +27,28 @@ def create_hemisphere(
 
     return mesh
 
+def decouple_config(config: dict):
+  decoupled_config = []
+  for key, value in config.items():
+    if key == "name":
+      continue
+    decoupled_config.append({
+      "parent_name": config["name"],
+      "name": config["name"] + "_" + key,
+      key: value.copy()
+    })
+
+  return decoupled_config
+
 def mesh_config_generator(
     name: str,
     geom_type: mujoco.mjtGeom,
-    size: np.ndarray
+    size: np.ndarray,
+    decouple: bool = False
 ):
 
     if geom_type == mujoco.mjtGeom.mjGEOM_PLANE:
-      return {
+      config = {
         "name": name,
         "box": {
           "width": size[0] * 2 if size[0] > 0 else 100,
@@ -44,7 +58,7 @@ def mesh_config_generator(
         }
       }
     elif geom_type == mujoco.mjtGeom.mjGEOM_SPHERE:
-      return {
+      config = {
         "name": name,
         "sphere": {
           "radius": float(size[0])
@@ -52,7 +66,7 @@ def mesh_config_generator(
       }
     elif geom_type == mujoco.mjtGeom.mjGEOM_CAPSULE:
       cylinder = mesh_config_generator(name, mujoco.mjtGeom.mjGEOM_CYLINDER, size)
-      return {
+      config = {
         "name": name,
         "cylinder": cylinder["cylinder"],
         "left_hemisphere": {
@@ -74,12 +88,12 @@ def mesh_config_generator(
       sphere["sphere"]["transform"] = {
         "scale": tuple(size)
       }
-      return {
+      config = {
         "name": name,
         "sphere": sphere["sphere"],
       }
     elif geom_type == mujoco.mjtGeom.mjGEOM_CYLINDER:
-      return {
+      config = {
         "name": name,
         "cylinder": {
           "radius": size[0],
@@ -87,7 +101,7 @@ def mesh_config_generator(
         }
       }
     elif geom_type == mujoco.mjtGeom.mjGEOM_BOX:
-      return {
+      config = {
         "name": name,
         "box": {
           "width": size[0] * 2,
@@ -98,6 +112,10 @@ def mesh_config_generator(
     else:
       raise NotImplemented(f"{geom_type} primitive geom type not implemented with USD integration")
 
+    if decouple:
+      config = decouple_config(config)
+
+    return config
 
 def mesh_generator(
     mesh_config: dict,
@@ -110,7 +128,7 @@ def mesh_generator(
 
     for shape, config in mesh_config.items():
 
-      if shape == "name":
+      if "name" in shape:
         continue
 
       if "box" in shape:
@@ -140,9 +158,9 @@ def mesh_generator(
         )
 
       if "transform" in config:
-
         if "rotate" in config["transform"]:
-          R = mesh.get_rotation_matrix_from_xyz(config["transform"]["rotate"])
+          tmp = o3d.geometry.TriangleMesh.create_coordinate_frame() # TODO: add rotation matrix code gen to utils.py
+          R = tmp.get_rotation_matrix_from_xyz(config["transform"]["rotate"])
           prim_mesh.rotate(R, center=(0, 0, 0))
         if "scale" in config["transform"]:
           prim_mesh.vertices = o3d.utility.Vector3dVector(

python/mujoco/usd/temp.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+from abs import ABC
 from typing import List, Optional, Tuple
 
 import mujoco
@@ -33,7 +34,6 @@ from pxr import UsdGeom
 from pxr import UsdLux
 from pxr import UsdShade
 from pxr import Vt
-
 
 class USDMesh:
 
@@ -265,15 +265,13 @@ class USDPrimitiveMesh:
     self.prim_mesh = None
     self.transform_op = Gf.Matrix4d(1.)
 
-    _, self.prim_mesh = shapes_component.mesh_generator(mesh_config)
+    self.prim_mesh = self.generate_primitive_mesh()
 
     xform_path = f"/World/{self.obj_name}_Xform"
     mesh_path = f"{xform_path}/{obj_name}"
     self.usd_xform = UsdGeom.Xform.Define(stage, xform_path)
     self.usd_mesh = UsdGeom.Mesh.Define(stage, mesh_path)
     self.usd_prim = stage.GetPrimAtPath(mesh_path)
-
-    self.prim_mesh.translate(-self.prim_mesh.get_center())
 
     mesh_vert, mesh_face, mesh_facenum = self._get_mesh_geometry()
     self.usd_mesh.GetPointsAttr().Set(mesh_vert)
@@ -301,6 +299,11 @@ class USDPrimitiveMesh:
     self.scale_op = self.usd_xform.AddScaleOp()
 
     self.last_visible_frame = -2
+
+  def generate_primitive_mesh(self):
+    _, prim_mesh = shapes_component.mesh_generator(self.mesh_config)
+    prim_mesh.translate(-prim_mesh.get_center())
+    return prim_mesh
 
   def _set_refinement_properties(self):
     self.usd_prim.GetAttribute("subdivisionScheme").Set("none")
@@ -417,7 +420,7 @@ class USDPrimitiveMesh:
     self.usd_mesh.GetPrim().ApplyAPI(UsdShade.MaterialBindingAPI)
     UsdShade.MaterialBindingAPI(self.usd_mesh).Bind(mtl)
 
-  def update(self, pos: np.ndarray, mat: np.ndarray, visible: bool, frame: int):
+  def update(self, pos: np.ndarray, mat: np.ndarray, visible: bool, frame: int, ):
     transformation_mat = utils_component.create_transform_matrix(
         rotation_matrix=mat, translation_vector=pos
     ).T
@@ -438,29 +441,12 @@ class USDPrimitiveMesh:
   def update_scale(self, scale: np.ndarray, frame: int):
     self.scale_op.Set(Gf.Vec3f(scale.tolist()), frame)
 
-class USDTendon(USDPrimitiveMesh):
-
+class USDTendon:
+  
   def __init__(
-    self,
-    mesh_config: List[dict],
-    stage: Usd.Stage,
-    geom: mujoco.MjvGeom,
-    obj_name: str,
-    rgba: np.ndarray = np.array([1, 1, 1, 1]),
-    texture_file: Optional[str] = None,
+      self
   ):
-    super().__init__(
-      mesh_config=mesh_config,
-      stage=stage,
-      geom=geom,
-      obj_name=obj_name,
-      rgba=rgba,
-      texture_file=texture_file
-    )
-
-  def update(self, pos: np.ndarray, mat: np.ndarray, scale: np.ndarray, visible: bool, frame: int):
-    super().update(pos, mat, visible, frame)
-    super().update_scale(scale, frame)
+    pass
 
 class USDSphereLight:
 