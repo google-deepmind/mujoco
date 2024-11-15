33708643c9528ce26167d9a2aa8ff7d9449bf247
Junhyeok Ahn <jahn@bostondynamics.com>
2024-11-15 11:20:18 -0500

    fix indentation

--- python/mujoco/__init__.py
+++ python/mujoco/__init__.py
@@ -14,16 +14,6 @@
 # ==============================================================================
 """Python bindings for MuJoCo."""
 
-from mujoco.renderer import Renderer
-from mujoco.gl_context import *
-from mujoco._structs import *
-from mujoco._specs import *
-from mujoco._render import *
-from mujoco._functions import *
-from mujoco._errors import *
-from mujoco._enums import *
-from mujoco._constants import *
-from mujoco._callbacks import *
 import ctypes
 import ctypes.util
 import os
@@ -42,39 +32,46 @@ __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
 _SYSTEM = platform.system()
 if _SYSTEM == 'Windows':
-    ctypes.WinDLL(os.path.join(os.path.dirname(__file__), 'mujoco.dll'))
+  ctypes.WinDLL(os.path.join(os.path.dirname(__file__), 'mujoco.dll'))
 elif _SYSTEM == 'Darwin':
-    proc_translated = subprocess.run(
-        ['sysctl', '-n', 'sysctl.proc_translated'], capture_output=True).stdout
-    try:
-        is_rosetta = bool(int(proc_translated))
-    except ValueError:
-        is_rosetta = False
-    if is_rosetta and platform.machine() == 'x86_64':
-        raise ImportError(
-            'You are running an x86_64 build of Python on an Apple Silicon '
-            'machine. This is not supported by MuJoCo. Please install and run a '
-            'native, arm64 build of Python.')
+  proc_translated = subprocess.run(
+      ['sysctl', '-n', 'sysctl.proc_translated'], capture_output=True).stdout
+  try:
+    is_rosetta = bool(int(proc_translated))
+  except ValueError:
+    is_rosetta = False
+  if is_rosetta and platform.machine() == 'x86_64':
+    raise ImportError(
+        'You are running an x86_64 build of Python on an Apple Silicon '
+        'machine. This is not supported by MuJoCo. Please install and run a '
+        'native, arm64 build of Python.')
 
+from mujoco._callbacks import *
+from mujoco._constants import *
+from mujoco._enums import *
+from mujoco._errors import *
+from mujoco._functions import *
+from mujoco._render import *
+from mujoco._specs import *
+from mujoco._structs import *
+from mujoco.gl_context import *
+from mujoco.renderer import Renderer
 
 HEADERS_DIR = os.path.join(os.path.dirname(__file__), 'include/mujoco')
 PLUGINS_DIR = os.path.join(os.path.dirname(__file__), 'plugin')
 
 PLUGIN_HANDLES = []
 
-
 def _load_all_bundled_plugins():
-    for directory, _, filenames in os.walk(PLUGINS_DIR):
-        for filename in filenames:
-            if os.path.splitext(filename)[-1] in [".dll", ".dylib", ".so"]:
-                PLUGIN_HANDLES.append(ctypes.CDLL(
-                    os.path.join(directory, filename)))
-            elif filename == "__init__.py":
-                pass
-            else:
-                warnings.warn('Ignoring non-library in plugin directory: '
-                              f'{os.path.join(directory, filename)}', ImportWarning)
-
+  for directory, _, filenames in os.walk(PLUGINS_DIR):
+    for filename in filenames:
+      if os.path.splitext(filename)[-1] in [".dll", ".dylib", ".so"]:
+        PLUGIN_HANDLES.append(ctypes.CDLL(os.path.join(directory, filename)))
+      elif filename == "__init__.py":
+        pass
+      else:
+        warnings.warn('Ignoring non-library in plugin directory: '
+                      f'{os.path.join(directory, filename)}', ImportWarning)
 
 _load_all_bundled_plugins()
 
