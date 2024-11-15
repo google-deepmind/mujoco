413de06a84e11b5008d6071b275177a7a4d65b11
Junhyeok Ahn <jahn@bostondynamics.com>
2024-11-15 11:18:30 -0500

    delete duplicated pkgutil

--- python/mujoco/__init__.py
+++ python/mujoco/__init__.py
@@ -14,8 +14,6 @@
 # ==============================================================================
 """Python bindings for MuJoCo."""
 
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
-
 import ctypes
 import ctypes.util
 import os
