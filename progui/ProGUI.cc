4d71feaafb33b3e2e91a6465622cce80c2ede7fc
raddy100 <ufmain@outlook.com>
2025-08-04 13:15:01 -0700

    adding a cmake list to stub out a new project within the Mujoco project.
    need to disable the other projects in the top level cmakelists to make compiling faster

---
+++ progui/ProGUI.cc
@@ -0,0 +1,14 @@
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <GLFW/glfw3.h>
+#include <mujoco/mujoco.h>
+
+int main(int argc, const char** argv) {
+
+
+
+	return EXIT_SUCCESS;
+}
