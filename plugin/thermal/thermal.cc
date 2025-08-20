6ed2a8184f5e2f6921ab3dd89fbb8927f29dd58f
Vidur <vidurvij@apptronik.com>
2025-08-20 13:55:33 -0500

    Define RegisterPlugin and the constructor functions

---
+++ plugin/thermal/thermal.cc
@@ -0,0 +1,112 @@
+#include "thermal.h"
+
+#include <cstdint>
+#include <cstdlib>
+#include <memory>
+#include <optional>
+#include <utility>
+#include <vector>
+
+#include <mujoco/mujoco.h>
+
+namespace mujoco::plugin::thermal {
+
+constexpr char kattrThermalCapacitance[] = "C";
+constexpr char kattrThermalResistance[] = "Rth";
+constexpr char kAttrElectricNominalResistance[] = "RNorm";
+constexpr char kTemperatureCoefficientofResisteance[] = "TempCoeff";
+constexpr char kAttrTorqueConstant25[] = "Kt25";
+constexpr char kAttrTorqueConstant130[] = "Kt130";
+constexpr char kAttrGearRatio[] = "G";
+
+std::optional<mjtNum> ReadOptionalDoubleAttr(const mjModel* m, int instance,
+                                             const char* attr) {
+  const char* value = mj_getPluginConfig(m, instance, attr);
+  if (value == nullptr || value[0] == '\0') {
+    return std::nullopt;
+  }
+  return std::strtod(value, nullptr);
+}
+
+ThermalConfig ThermalConfig::FromModel(const mjModel* m, int instance) {
+  ThermalConfig config;
+  config.thermal_capacitance = ReadOptionalDoubleAttr(m, instance, kattrThermalCapacitance).value_or(0);
+  config.thermal_resistance = ReadOptionalDoubleAttr(m, instance, kattrThermalResistance).value_or(0);
+  config.electrical_nominal_resistance = ReadOptionalDoubleAttr(m, instance, kAttrElectricNominalResistance).value_or(0);
+  config.temperature_coefficient_of_resistance = ReadOptionalDoubleAttr(m, instance, kTemperatureCoefficientofResisteance).value_or(0);
+  config.torque_constant_25 = ReadOptionalDoubleAttr(m, instance, kAttrTorqueConstant25).value_or(0);
+  config.torque_constant_130 = ReadOptionalDoubleAttr(m, instance, kAttrTorqueConstant130).value_or(0);
+  config.gear_ratio = ReadOptionalDoubleAttr(m, instance, kAttrGearRatio).value_or(0);
+
+  return config;
+}
+
+void Thermal::RegisterPlugin() 
+{
+  mjpPlugin plugin;
+  mjp_defaultPlugin(&plugin);
+  plugin.name = "mujoco.thermal";
+  plugin.capabilityflags |= mjPLUGIN_ACTUATOR;
+  plugin.needstage = mjSTAGE_ACC;
+
+
+  std::vector<const char *> attributes = {kattrThermalCapacitance,
+                                          kattrThermalResistance,
+                                          kAttrElectricNominalResistance,
+                                          kTemperatureCoefficientofResisteance,
+                                          kAttrTorqueConstant25,
+                                          kAttrTorqueConstant130,
+                                          kAttrGearRatio};
+  plugin.nattribute = attributes.size();
+  plugin.attributes = attributes.data();
+
+  // Can only run after forces have been computed.
+  plugin.needstage = mjSTAGE_ACC;
+
+  plugin.nstate = +[](const mjModel *m, int instance) {
+    return 1;
+  }; // We are keeping the state size as 1 to record the true temperature. And
+     // the sensor output as "observed temperature". In the future we can add a
+     // sensor noise model to this.
+  plugin.nsensordata = +[](const mjModel *m, int instance, int sensor_id) {
+    return 1; // The sensor output is the observed temperature.
+  };
+
+  plugin.init = +[](const mjModel *m, mjData *d, int instance) {
+    std::unique_ptr<Thermal> thermal = Thermal::Create(m, instance);
+    if (thermal == nullptr) {
+      return -1;
+    }
+    d->plugin_data[instance] = reinterpret_cast<uintptr_t>(thermal.release());
+    return 0;
+  };
+
+  plugin.destroy = +[](mjData *d, int instance) {
+    delete reinterpret_cast<Thermal *>(d->plugin_data[instance]);
+    d->plugin_data[instance] = 0;
+  };
+  plugin.reset = +[](const mjModel *m, mjtNum *plugin_state, void *plugin_data,
+                     int instance) {
+    auto *thermal = reinterpret_cast<Thermal *>(plugin_data);
+    thermal->Reset(plugin_state);
+  };
+
+  plugin.compute =
+      +[](const mjModel *m, mjData *d, int instance, int capability_bit) {
+        auto *thermal = reinterpret_cast<Thermal *>(d->plugin_data[instance]);
+        thermal->Compute(m, d, instance);
+      };
+  mjp_registerPlugin(&plugin);
+}
+
+Thermal::Thermal(ThermalConfig config, std::vector<int> actuators)
+    : _model(std::move(config)), _actuators(std::move(actuators)) {}
+
+std::unique_ptr<Thermal> Thermal::Create(const mjModel *m, int instance) 
+{
+    ThermalConfig config = ThermalConfig::FromModel(m, instance);
+    std::vector<int> actuators; // TODO: populate actuator list
+    return std::unique_ptr<Thermal>(new Thermal(config, actuators));
+}
+
+} // namespace mujoco::plugin::thermal
