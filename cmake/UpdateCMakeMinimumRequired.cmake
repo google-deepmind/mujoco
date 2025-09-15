c79b2d064b0321924c054b910867464bcb14b51e
Taylor Howell <taylorhowell@google.com>
2025-09-15 12:17:41 +0100

    add description of file utility

--- cmake/UpdateCMakeMinimumRequired.cmake
+++ cmake/UpdateCMakeMinimumRequired.cmake
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Replace cmake_minimum_required version.
+
 file(READ "${TARGET_FILE}" FILE_CONTENTS)
 
 string(REGEX REPLACE
