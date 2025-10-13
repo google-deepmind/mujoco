02d2d4d49aa4927a146a2a8e4fb42833bd92488e
Gabe Oppenheimer <goppenheimer@google.com>
2025-10-13 09:21:48 -0700

    Bump the version to 3.4.0 ahead of the next release.
    PiperOrigin-RevId: 818694877
    Change-Id: I42e261956a0219e549bec6a54cbf330a74f02f9f

--- src/engine/engine_support.c
+++ src/engine/engine_support.c
@@ -41,8 +41,8 @@
 
 //-------------------------- Constants -------------------------------------------------------------
 
- #define mjVERSION 337
-#define mjVERSIONSTRING "3.3.7"
+ #define mjVERSION 340
+#define mjVERSIONSTRING "3.4.0"
 
 // names of disable flags
 const char* mjDISABLESTRING[mjNDISABLE] = {
