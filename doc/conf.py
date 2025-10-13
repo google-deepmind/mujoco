d9399d6f847833a5e93f0c0c67c44ce9566689ce
Taylor Howell <taylorhowell@google.com>
2025-10-13 01:46:23 -0700

    Update doc/conf.py.
    PiperOrigin-RevId: 818544892
    Change-Id: I4bb14309ebb4abe9eca1a61fa91b1bba6992a1e5

--- doc/conf.py
+++ doc/conf.py
@@ -25,9 +25,6 @@ import sys
 sys.path.insert(0, os.path.abspath('../'))
 sys.path.append(os.path.abspath('ext'))
 
-# MuJoCo Warp
-sys.path.append(os.path.abspath('../../mujoco_warp'))
-
 from sphinxcontrib import katex  # pylint: disable=g-import-not-at-top
 from sphinxcontrib import youtube  # pylint: disable=g-import-not-at-top,unused-import
 
