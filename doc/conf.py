baa5f2f39eb4cf7bddfd698337180075f1b5bd4f
Taylor Howell <taylorhowell@google.com>
2025-10-17 13:49:47 +0100

    update

--- doc/conf.py
+++ doc/conf.py
@@ -22,9 +22,7 @@ import sys
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
-sys.path.insert(0, os.path.abspath('../'))
-sys.path.append(os.path.abspath('ext'))
-sys.path.insert(0, os.path.abspath('../mjx'))
+sys.path.insert(0, os.path.abspath('ext'))
 sys.path.insert(0, os.path.abspath('../mjx/mujoco/mjx/third_party'))
 
 from sphinxcontrib import katex  # pylint: disable=g-import-not-at-top
