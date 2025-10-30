cc43876ea30f54dc2384e736ffdb3ad5efbc2e37
Matija Kecman <matijak@google.com>
2025-10-30 11:58:04 +0000

    Update version, simplify test, shuffle github actions

--- wasm/tests/run-tests.mjs
+++ wasm/tests/run-tests.mjs
@@ -1,28 +1,26 @@
 import Jasmine from 'jasmine';
 
-(async () => {
-  const jasmine = new Jasmine();
+const jasmine = new Jasmine();
 
-  jasmine.loadConfig({
-    spec_dir: 'tests',
-    spec_files: ['**/*_test.ts'],
-    jsLoader: 'import',
-    random: false,
-    stopSpecOnExpectationFailure: false,
-  });
+jasmine.loadConfig({
+  spec_dir: 'tests',
+  spec_files: ['**/*_test.ts'],
+  jsLoader: 'import',
+  random: false,
+  stopSpecOnExpectationFailure: false,
+});
 
-  try {
-    console.log('Starting Jasmine test run...');
-    const result = await jasmine.execute();
-    console.log(`Jasmine test run finished. Status: ${result.overallStatus}`);
+try {
+  console.log('Starting Jasmine test run...');
+  const result = await jasmine.execute();
+  console.log(`Jasmine test run finished. Status: ${result.overallStatus}`);
 
-    if (result.overallStatus === 'failed') {
-      process.exit(1);
-    }
-
-  } catch (error) {
-    console.error('Test runner script failed:', error);
+  if (result.overallStatus === 'failed') {
     process.exit(1);
   }
-})();
+
+} catch (error) {
+  console.error('Test runner script failed:', error);
+  process.exit(1);
+}
 
