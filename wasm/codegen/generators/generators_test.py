9b71fc728d6c2d7546df2d75f1fe45969fd80c2d
Matija Kecman <matijak@google.com>
2025-11-04 03:18:43 -0800

    Consolidate WASM bindings test files
    PiperOrigin-RevId: 827888382
    Change-Id: I4ded54cd49182de0b4243770e2507038ad6fac7a

--- wasm/codegen/generators/generators_test.py
+++ wasm/codegen/generators/generators_test.py
@@ -17,6 +17,7 @@ from absl.testing import absltest
 from introspect import ast_nodes
 
 from wasm.codegen.generators import enums
+from wasm.codegen.generators import functions
 
 
 class EnumsGeneratorTest(absltest.TestCase):
@@ -56,6 +57,49 @@ class EnumsGeneratorTest(absltest.TestCase):
     actual_code = generator.generate()
 
     self.assertEqual(actual_code, expected_code)
+
+class FunctionsGeneratorTest(absltest.TestCase):
+
+  def setUp(self):
+    super().setUp()
+    self.generator = functions.Generator({})
+    self.int_type = ast_nodes.ValueType(name="int")
+
+  def test_generate_function_binding_simple_case(self):
+    func_simple_void = ast_nodes.FunctionDecl(
+        name="do_nothing",
+        return_type=ast_nodes.ValueType(name="void"),
+        parameters=tuple(),
+        doc="doc",
+    )
+    self.assertEqual(
+        self.generator._generate_function_binding(func_simple_void),
+        'function("do_nothing", &do_nothing);\n',
+    )
+
+  def test_generate_direct_bindable_functions_simple_filter(self):
+    direct_bind = ast_nodes.FunctionDecl(
+        name="direct_bind",
+        return_type=self.int_type,
+        parameters=(
+            ast_nodes.FunctionParameterDecl(name="val", type=self.int_type),
+        ),
+        doc="doc",
+    )
+    needs_wrap = ast_nodes.FunctionDecl(
+        name="needs_wrap",
+        return_type=ast_nodes.PointerType(inner_type=self.int_type),
+        parameters=tuple(),
+        doc="doc",
+    )
+    self.generator = functions.Generator({
+        "direct1": direct_bind,
+        "wrapped1": needs_wrap,
+    })
+
+    generated_code = self.generator._generate_direct_bindable_functions()
+    self.assertIn('function("direct_bind", &direct_bind);\n', generated_code)
+    self.assertNotIn('function("needs_wrap", &needs_wrap);\n', generated_code)
 
 
 if __name__ == "__main__":
