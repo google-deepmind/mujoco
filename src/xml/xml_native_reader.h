da36e822a8abf6ff6b88fb2768857a76f8fad0db
Haroon Qureshi <haroonq@google.com>
2025-12-04 08:22:05 -0800

    Allow material default classes to define texture layers.
    PiperOrigin-RevId: 840254100
    Change-Id: I59f67fc4d1485cf80bbc005d1b35cda6224248ed

--- src/xml/xml_native_reader.h
+++ src/xml/xml_native_reader.h
@@ -102,7 +102,7 @@ class mjXReader : public mjXBase {
 };
 
 // MJCF schema
-#define nMJCF 241
+#define nMJCF 244
 extern std::vector<const char*> MJCF[nMJCF];
 
 #endif  // MUJOCO_SRC_XML_XML_NATIVE_READER_H_
