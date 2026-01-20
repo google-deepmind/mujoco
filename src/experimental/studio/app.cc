a0accc493a8879e1cf5c80753b6cb62299a20118
Haroon Qureshi <haroonq@google.com>
2026-01-20 08:17:30 -0800

    Fix camera select logic.
    PiperOrigin-RevId: 858599199
    Change-Id: I25f1a74ac145c73bb225d6df8e86aa82d88ebb4b

--- src/experimental/studio/app.cc
+++ src/experimental/studio/app.cc
@@ -1272,7 +1272,7 @@ void App::ToolBarGui() {
       for (int n = 0; n < cameras.size(); n++) {
         if (ImGui::Selectable(cameras[n], (camera_idx == n))) {
           ui_.camera_idx = platform::SetCamera(
-              model_, &camera_, camera_idx + platform::kTumbleCameraIdx);
+              model_, &camera_, n + platform::kTumbleCameraIdx);
         }
       }
       ImGui::EndCombo();
