15f61679bf90f70be5ce392b39c1f4039b041b61
Michael Moss <mmoss@google.com>
2026-04-23 09:32:20 -0700

    Update next MuJoCo version to 3.8.0 for pending breaking changes.
    PiperOrigin-RevId: 904496070
    Change-Id: I0b8b01cbcf27f28ac80dbc283bc63090f745f403

--- src/engine/engine_support.c
+++ src/engine/engine_support.c
@@ -43,8 +43,8 @@
 
 //-------------------------- Constants -------------------------------------------------------------
 
- #define mjVERSION 3007001
-#define mjVERSIONSTRING "3.7.1"
+ #define mjVERSION 3008000
+#define mjVERSIONSTRING "3.8.0"
 
 // names of disable flags
 const char* mjDISABLESTRING[mjNDISABLE] = {
