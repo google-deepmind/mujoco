775f1186d9a6a1b29939753e963326322059ab73
Lou Rohan <lrohan@nvidia.com>
2026-06-30 10:49:48 -0500

    Parse disabled collider metadata in USD decoder

--- plugin/usd_decoder/usd_decoder.cc
+++ plugin/usd_decoder/usd_decoder.cc
@@ -1921,6 +1921,10 @@ void ParseUsdGeomGprim(mjSpec* spec, const pxr::UsdPrim& gprim,
     }
   }
 
+  if (gprim.HasAPI<pxr::MjcPhysicsCollisionAPI>()) {
+    ParseMjcPhysicsCollisionAPI(geom, pxr::MjcPhysicsCollisionAPI(gprim));
+  }
+
   if (gprim.HasAPI<pxr::MjcPhysicsImageableAPI>()) {
     auto imageable_api = pxr::MjcPhysicsImageableAPI(gprim);
     auto group_attr = imageable_api.GetGroupAttr();
