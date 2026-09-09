f4959d9dc173cbe61adcb9fb5bfb76040264bccd
Michael Moss <mmoss@google.com>
2026-09-09 03:26:23 -0700

    Update MuJoCo version to 3.13.1 following the 3.13.0 release
    PiperOrigin-RevId: 978439068
    Change-Id: Ia0511eab87e1b82a2775ce928f4aa8c7f299e65c

--- src/engine/engine_support.c
+++ src/engine/engine_support.c
@@ -43,8 +43,8 @@
 
 //-------------------------- Constants -------------------------------------------------------------
 
- #define mjVERSION 3013000
-#define mjVERSIONSTRING "3.13.0"
+ #define mjVERSION 3013001
+#define mjVERSIONSTRING "3.13.1"
 
 // names of disable flags
 const char* mjDISABLESTRING[mjNDISABLE] = {
